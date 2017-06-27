0e04dbb91280154b7501f84e57a5214d156ce8d9
Peter Pfläging <peter@pflaeging.net>
2017-06-27 14:12:47 +0200

    New plugin: redmine_task_board Changed TOP Menu!

--- init.rb
+++ init.rb
@@ -6,15 +6,8 @@ Redmine::Plugin.register :stickie_flow do
   url 'http://www.stickieflow.net'
   author_url 'http://www.pflaeging.net'
   description "A Redmine plugin to adapt the behaviour to work as a Stickie~Flow support system"
-  version '0.5.0'
+  version '0.6.0'
 
   requires_redmine :version_or_higher => '0.8.7'
 
-  menu :top_menu, :agile, {:controller => 'agile_boards', :action => 'index' },
-                              :caption => :label_agile,
-                              :before => :projects
-
-  menu :top_menu, :issues, {:controller => 'issues', :action => 'new' },
-                              :caption => :label_issue_new,
-                              :before => :projects
 end
